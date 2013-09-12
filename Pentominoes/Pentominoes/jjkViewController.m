2f5621cc1ba4e2a4e10254596d613218e84c61b8
Joshua Kuiros <joshkuiros@gmail.com>
2013-09-11 22:23:06 -0400

    app nearly fully functioning, need to enabled/disable buttons

--- Pentominoes/Pentominoes/jjkViewController.m
+++ Pentominoes/Pentominoes/jjkViewController.m
@@ -9,25 +9,24 @@
 #import "jjkViewController.h"
 #define yOffset 110
 #define xOffset 10
+#define squareDimension 30
 
 @interface jjkViewController ()
 - (IBAction)solveButtonPressed:(id)sender;
+- (IBAction)resetButtonPressed:(id)sender;
+@property (weak, nonatomic) IBOutlet UIButton *resetButton;
 @property (weak, nonatomic) IBOutlet UIImageView *boardImageView;
-@property (nonatomic, strong) NSArray *puzzlePieceArray;
+@property (nonatomic, strong) NSMutableDictionary *puzzlePieceDictionary;
 @property NSInteger currentBoardSelected;
 @end
 
+
 @implementation jjkViewController
 
--(NSDictionary*)dictionaryWithImageName:(NSString*)imageName {
-    UIImage *image = [UIImage imageNamed:imageName];
-    NSDictionary *dict = @{@"image":image};
-    return dict;
-}
 
 -(void)displayPuzzlePieces
 {
-    self.puzzlePieceArray = [self initializePuzzlePieces];
+    
     
     CGPoint startingPoint = self.boardImageView.frame.origin;
     startingPoint.y += self.boardImageView.frame.size.height;
@@ -40,31 +39,34 @@
     //NSInteger screenHeight = [UIScreen mainScreen].bounds.size.height;
     
     
+
     
-    
-    
-    for(UIImageView *puzzlePiece in self.puzzlePieceArray)
+    for(id key in self.puzzlePieceDictionary)
     {
-        if(currentPoint.x + puzzlePiece.image.size.width >= screenWidth)
+        UIImageView *temporaryImageView = [[self.puzzlePieceDictionary objectForKey:key] objectForKey:@"ImageView"];
+       // NSNumber *temporaryRotations = [[self.puzzlePieceDictionary objectForKey:key] objectForKey:@"rotations"];
+        //NSNumber *temporaryFlips = [[self.puzzlePieceDictionary objectForKey:key] objectForKey:@"flips"];
+        
+        if(currentPoint.x + temporaryImageView.frame.size.width >= screenWidth)
         {
             currentPoint.x = startingPoint.x;
             currentPoint.y += yOffset;
             
-            puzzlePiece.frame = CGRectMake(currentPoint.x, currentPoint.y, (puzzlePiece.image.size.width)/2, (puzzlePiece.image.size.height)/2);
+            temporaryImageView.frame = CGRectMake(currentPoint.x, currentPoint.y, temporaryImageView.frame.size.width, temporaryImageView.frame.size.height);
             
-            currentPoint.x += puzzlePiece.image.size.width/2 + xOffset;
+            currentPoint.x += temporaryImageView.image.size.width + xOffset;
             
             
         }
         else
         {
-            puzzlePiece.frame = CGRectMake(currentPoint.x, currentPoint.y, puzzlePiece.image.size.width/2, puzzlePiece.image.size.height/2);
-            currentPoint.x += puzzlePiece.image.size.width/2 + xOffset;
+            temporaryImageView.frame = CGRectMake(currentPoint.x, currentPoint.y, temporaryImageView.frame.size.width, temporaryImageView.frame.size.height);
+            currentPoint.x += temporaryImageView.image.size.width/2 + xOffset;
             
             
         }
         
-        [self.view addSubview:puzzlePiece];
+        [self.view addSubview:temporaryImageView];
         
     }
 
@@ -80,7 +82,13 @@
 
 -(void)viewDidAppear:(BOOL)animated
 {
+    
+    _puzzlePieceDictionary = [self initializePuzzlePieces];
+    [UIView animateWithDuration:1 animations:^{
     [self displayPuzzlePieces];
+    }];
+    
+    self.resetButton.enabled = NO;
 }
 
 - (void)didReceiveMemoryWarning
@@ -89,33 +97,33 @@
     // Dispose of any resources that can be recreated.
 }
 
--(NSArray *)initializePuzzlePieces
+-(NSMutableDictionary*)initializePuzzlePieces
 {
     NSArray *initialPuzzlePieceArray = [NSArray arrayWithObjects:@"tileF.png",@"tileI.png",@"tileL.png",@"tileN.png",@"tileP.png",@"tileT.png",@"tileU.png",@"tileV.png",@"tileW.png",@"tileX.png",@"tileY.png",@"tileZ.png", nil];
     
-    NSMutableArray *imageViewArray = [NSMutableArray array];
+    NSMutableDictionary *puzzlePiecesDictionary = [NSMutableDictionary dictionary];
+    
+    NSRange keyRange;
+    keyRange.length = 1;
+    keyRange.location = 4;
     
     for(NSString *path in initialPuzzlePieceArray)
     {
         UIImage *image = [UIImage imageNamed:path];
+        NSMutableDictionary *propertiesDictionary = [NSMutableDictionary dictionary];
         
-        CGRect resizeFrame;
-        resizeFrame.size.height = image.size.height/*/2*/;
-        resizeFrame.size.width = image.size.width/*/2*/;
+        UIImageView *temporaryPuzzleImageView = [[UIImageView alloc] initWithImage:image];
+        temporaryPuzzleImageView.frame = CGRectMake(0,0, image.size.width/2, image.size.height/2);
         
-        UIImageView *temporaryPuzzleImageView = [[UIImageView alloc] initWithFrame: resizeFrame];
-        temporaryPuzzleImageView.image = image;
-        
-        [imageViewArray addObject:temporaryPuzzleImageView];
+        [propertiesDictionary setObject:temporaryPuzzleImageView forKey:@"ImageView" ];
+        [puzzlePiecesDictionary setObject:propertiesDictionary forKey:[path substringWithRange:keyRange]];
         
     }
     
-    return imageViewArray;
-    
-    
-    
-    
+    return puzzlePiecesDictionary;
 }
+
+
 
 -(IBAction)boardButtonPressed:(id)sender
 {
@@ -125,26 +133,123 @@
     
     self.currentBoardSelected = [sender tag];
     
+    self.resetButton.enabled = NO;
+    
+    
 }
 
 - (IBAction)solveButtonPressed:(id)sender
 {
     NSBundle *bundle = [NSBundle mainBundle];
     NSString *solutionFile = [bundle pathForResource:@"Solutions" ofType:@".plist"];
-    
     NSArray *solutionsArray = [NSArray arrayWithContentsOfFile:solutionFile];
+    NSDictionary *boardDictionary;
     
-    NSNumber *boardSelected = [NSNumber numberWithInt:(_currentBoardSelected -1 );
     
     if(self.currentBoardSelected != 0)
     {
-        NSDictionary *boardDictioanry = [solutionsArray objectAtIndex:(self.currentBoardSelected - 1)];
-        for(NSString *puzzlePieces in puzzlePieceArray)
-        {
-            NSString *puzzleKey = [puzzlePieces substringWithRange:NSMakeRange(4,4)];
-            NSDictionary *puzzlePieceDictioanry = boardDictionary objectAtKey:@"";
-        }
+        self.resetButton.enabled = YES;
     }
-                               
+    else
+    {
+        self.resetButton.enabled = NO;
+    }
+    
+    
+    
+    if(self.currentBoardSelected == 0)
+    {
+        return;
+    }
+    
+    boardDictionary = solutionsArray[self.currentBoardSelected - 1];
+    
+    for(id key in self.puzzlePieceDictionary)
+    {
+        UIImageView *currentImageView = [[self.puzzlePieceDictionary objectForKey:key] objectForKey:@"ImageView"];
+        NSDictionary *pieceDictionary = [boardDictionary objectForKey:key];
+        NSInteger xCoordinate = [[pieceDictionary objectForKey:@"x"] integerValue];
+        NSInteger yCoordinate = [[pieceDictionary objectForKey:@"y"] integerValue];
+        
+        CGFloat x = self.boardImageView.frame.origin.x;
+        x += xCoordinate*squareDimension;
+       
+        CGFloat y = self.boardImageView.frame.origin.y;
+        y += yCoordinate*squareDimension;
+        
+        NSInteger rotations = [[pieceDictionary objectForKey:@"rotations"] integerValue];
+        NSInteger flips = [[pieceDictionary objectForKey:@"flips"] integerValue];
+        
+        
+        [UIView animateWithDuration:1 animations:^{
+        currentImageView.transform = CGAffineTransformRotate(currentImageView.transform, rotations * M_PI/2);
+        
+        if(flips == 1)
+        {
+            currentImageView.transform = CGAffineTransformScale(currentImageView.transform, -1, 1);
+        }
+        
+        currentImageView.frame = CGRectMake(x, y, currentImageView.frame.size.width, currentImageView.frame.size.height);
+        
+        [self.view addSubview:currentImageView];
+        }];
+    
+    }
+    
+
+                                  
 }
+
+- (IBAction)resetButtonPressed:(id)sender
+{
+    NSBundle *bundle = [NSBundle mainBundle];
+    NSString *solutionFile = [bundle pathForResource:@"Solutions" ofType:@".plist"];
+    NSArray *solutionsArray = [NSArray arrayWithContentsOfFile:solutionFile];
+    NSDictionary *boardDictionary;
+    
+    self.resetButton.enabled = NO;
+    
+    if(self.currentBoardSelected == 0)
+    {
+        return;
+    }
+    
+    boardDictionary = solutionsArray[self.currentBoardSelected - 1];
+    
+    for(id key in self.puzzlePieceDictionary)
+    {
+        UIImageView *currentImageView = [[self.puzzlePieceDictionary objectForKey:key] objectForKey:@"ImageView"];
+        NSDictionary *pieceDictionary = [boardDictionary objectForKey:key];
+        NSInteger xCoordinate = [[pieceDictionary objectForKey:@"x"] integerValue];
+        NSInteger yCoordinate = [[pieceDictionary objectForKey:@"y"] integerValue];
+        
+        CGFloat x = self.boardImageView.frame.origin.x;
+        x += xCoordinate*squareDimension;
+        
+        CGFloat y = self.boardImageView.frame.origin.y;
+        y += yCoordinate*squareDimension;
+        
+        NSInteger rotations = [[pieceDictionary objectForKey:@"rotations"] integerValue];
+        NSInteger flips = [[pieceDictionary objectForKey:@"flips"] integerValue];
+        
+        
+       [UIView animateWithDuration:1 animations:^{
+        if(flips == 1)
+        {
+            currentImageView.transform = CGAffineTransformScale(currentImageView.transform, -1, 1);
+        }
+        
+        currentImageView.transform = CGAffineTransformRotate(currentImageView.transform, -(rotations * M_PI/2));
+        
+        currentImageView.frame = CGRectMake(x, y, currentImageView.frame.size.width, currentImageView.frame.size.height);
+        
+       }];
+        
+    }    
+    
+    
+    [UIView animateWithDuration:1 animations:^{
+    [self displayPuzzlePieces];
+    }];
+    }
 @end
